0f0bbf9788723a9e5e253c0930255f02db6d882f
Bill <Bill@TheWallisFamily.co.uk>
2023-08-09 17:14:03 +0100

    ✨ feat: Add a `pascal_to_snake` function

--- src/daily_tracker/utils.py
+++ src/daily_tracker/utils.py
@@ -6,6 +6,34 @@ import pathlib
 ROOT = pathlib.Path(__file__).parent  # resolve to `src/daily_tracker`
 
 
+def pascal_to_snake(text: str) -> str:
+    """
+    Convert a pascal-case string to a snake-case string.
+
+    Adapted from:
+
+    - https://stackoverflow.com/a/44969381/8213085
+
+    Note that this splits on *every* uppercase letter, including consecutive
+    ones::
+
+        >>> pascal_to_snake(text="HTTP")
+        'h_t_t_p'
+
+    :param text: The pascal-case string to convert into a snake-case string. A
+     pascal-case string has a capital letter at the start of each word with no
+     separator between words, such as "PascalCase".
+
+    :return: The snake-case version of the input string.
+    """
+    return "".join(
+        [
+            f"_{letter.lower()}" if letter.isupper() else letter
+            for letter in text
+        ]
+    ).strip("_")
+
+
 def string_list_to_list(string_list: str, sep: str = ",") -> list:
     """
     Convert a string list to a Python list by splitting on the separator.
