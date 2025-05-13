70881d0bbb8dc9c22e40415444a00206729d694f
Bill Wallis <bill@tasman.ai>
2025-05-13 16:29:37 +0100

    feat: add non-debug entrypoint

--- daily_tracker/main.py
+++ daily_tracker/main.py
@@ -61,3 +61,7 @@ def main(debug_mode: bool = False) -> None:
         indefinite_scheduler.schedule_first()
 
     logging.info("Closing tracker...")
+
+
+if __name__ == "__main__":
+    main(debug_mode=False)
