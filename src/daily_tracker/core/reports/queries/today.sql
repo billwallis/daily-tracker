080d6c8507df27ce4ea99d0748b0e1f531cd824f
Bill Wallis <bill@tasman.ai>
2026-03-17 18:53:30 +0000

    fix: exclude lunch breaks in daily report

--- src/daily_tracker/core/reports/queries/today.sql
+++ src/daily_tracker/core/reports/queries/today.sql
@@ -4,6 +4,8 @@ select
     detail,
     (to_minutes(sum(interval)::int)::text)[0:5] as duration
 from tracker.tracker
-where date_time::date = current_date
+where 1=1
+    and task != 'Lunch Break'
+    and date_time::date = current_date
 group by grouping sets ((task, detail), ())
 order by task nulls last, detail nulls last
